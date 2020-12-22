469060c55c7bb577dd84e852407acce3d84bd314
Betül Gülcen <betulgulcen@stu.comu.edu.tr>
2020-12-22 14:45:32 +0300

    Create Set-SymmetricDifference-Operations.py

---
+++ Set-SymmetricDifference-Operations.py
@@ -0,0 +1,7 @@
+English = int(input())
+EnglishNumbers = set(map(int, input().split()))
+
+French = int(input())
+FrenchNumbers = set(map(int, input().split()))
+
+print(len(EnglishNumbers.symmetric_difference(FrenchNumbers)))
